d0cd33d9bc7c6010edd86cb3b8fb48a9b775b2de
JuanTabaresM <jjtabaresm@uqvirtual.edu.co>
2022-06-05 10:47:04 -0500

    Conexión a la API

---
+++ proy.py
@@ -0,0 +1,8 @@
+import tweepy
+import json
+
+consumer_key = "thEcmGVGHNYFaC9uDHDhiw6rv"
+sonsumer_secret = "u1CNJLc1flHxzC4R5Dvvk3y1DhwtUpPm3GN71OSZljDHoXLsns"
+acces_token = "1519004103870926853-1wGvzY3rFRz0XYtOgv4jEMupupfnVU"
+acces_token_secret = "odT5A9I5FRb9ZtyZARYROsp3riHr2yfnbrxOQiDMrcMqp"
+
